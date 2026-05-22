718f311febdb421557b39c983dceddaa34e06369
Wrydrick Gutierrez <122136179+wrydgutz@users.noreply.github.com>
2026-05-22 11:22:28 +0200

    Add comments

--- clrs/insertion-sort/main.cpp
+++ clrs/insertion-sort/main.cpp
@@ -5,16 +5,23 @@ using namespace std;
 
 void insertion_sort(vector<int>& nums) {
 
-    for (int i = 1; i < nums.size(); i++) {
+    // Traverse nums but starting from the 2nd number.
+    // The 1st element is already considered sorted by itself.
+    for (size_t i = 1; i < nums.size(); i++) {
 
-        int key = nums[i];
-        int j = i - 1;
+        // Keep track of the current number.
+        const int key = nums[i];
 
+        // Traverse backwards from the current element's previous index,
+        // shifting larger elements one position to the right
+        // until the correct insertion position for key is found.
+        size_t j = i - 1;
         while (j >= 0 && nums[j] > key) {
             nums[j + 1] = nums[j];
             j--;
         }
 
+        // Insert the key into its correct sorted position.
         nums[j + 1] = key;
     }
 }
@@ -28,10 +35,12 @@ void print(vector<int>& nums) {
 
 int main() {
     vector<int> a { 9, 34, 3, 2, 1, 6, 7 };
+
     cout << "Before sort: a = ";
     print(a);
     cout << endl;
     insertion_sort(a);
+
     cout << "After sort: a = ";
     print(a);
     cout << endl;
